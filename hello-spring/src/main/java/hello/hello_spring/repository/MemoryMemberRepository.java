b2f5bf8837ed1b5f420533c2cebefe65b9b63b48
Insik Jeong <150884733+InSik-j@users.noreply.github.com>
2024-05-31 16:41:04 +0900

    회원 관리 예제 - 백엔드 개발 / 회원 리포지토리 테스트 케이스 작성

--- hello-spring/src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -13,7 +13,6 @@ public class MemoryMemberRepository implements MemberRepository{
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
 
-
     @Override
     public Member save(Member member) {
         member.setId(++sequence);
@@ -36,5 +35,9 @@ public class MemoryMemberRepository implements MemberRepository{
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
     }
 }
