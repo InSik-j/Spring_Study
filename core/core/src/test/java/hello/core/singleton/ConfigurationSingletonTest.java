1be3f4e3f167b141457b6664bf14d991063eb591
Insik Jeong <150884733+InSik-j@users.noreply.github.com>
2024-06-13 15:40:24 +0900

    의존관계 자동 주입 / @Autowired 필드 명, @Qualifier, @Primary

--- core/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ core/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -21,15 +21,15 @@ public class ConfigurationSingletonTest {
         MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
 
         MemberRepository memberRepository1 = memberService.getMemberRepository();
-        MemberRepository memberRepository2 = orderService.getMemberRepository();
+        //MemberRepository memberRepository2 = orderService.getMemberRepository();
 
         //모두 같은 인스턴스를 참고하고 있다.
         System.out.println("memberService = " + memberRepository1);
-        System.out.println("orderService = " + memberRepository2);
+        //System.out.println("orderService = " + memberRepository2);
         System.out.println("memberRepository = " + memberRepository);
 
         assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
-        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+        //assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
 
     @Test
