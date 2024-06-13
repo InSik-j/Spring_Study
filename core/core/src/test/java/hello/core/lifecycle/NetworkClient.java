a623ad9b9121b70eba8319ba3eb98ecd60ba1d46
Insik Jeong <150884733+InSik-j@users.noreply.github.com>
2024-06-13 16:47:26 +0900

    빈 생명주기 콜백 / 빈 등록 초기화, 소멸 메서드 지정

--- core/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,7 +3,7 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient{
 
     private String url;
 
@@ -15,6 +15,7 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         this.url = url;
     }
 
+    //서비스 시작시 호출
     public void connect() {
         System.out.println("connect : "+ url);
     }
@@ -23,18 +24,19 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("call : "+ url + "message : "+ msg);
     }
 
+    //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close : "+ url);
     }
 
-    @Override
-    public void destroy() throws Exception {
-        disconnect();
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init(){
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
+    }
+
+    public void close(){
+        System.out.println("NetworkClient.close");
+        disconnect();
     }
 }
