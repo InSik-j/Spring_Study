e35176099e2f999bc2d1d51d228eddf0086b8779
Insik Jeong <150884733+InSik-j@users.noreply.github.com>
2024-07-03 14:48:04 +0900

    검증2 - Bean Validation / Form 전송 객체 분리 - 개발

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -11,20 +11,26 @@ import javax.validation.constraints.NotNull;
 @Data
 //@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10,000원이 넘어야 합니다.")
 public class Item {
-    
-    @NotNull (groups = UpdateCheck.class)// 수정시에만 적용
+
     private Long id;
-
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
-
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min=1000, max=1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
-
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = SaveCheck.class) // 등록시에만 적용
     private Integer quantity;
+
+
+//    @NotNull (groups = UpdateCheck.class)// 수정시에만 적용
+//    private Long id;
+//
+//    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+//    private String itemName;
+//
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min=1000, max=1000000, groups = {SaveCheck.class, UpdateCheck.class})
+//    private Integer price;
+//
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(value = 9999, groups = SaveCheck.class) // 등록시에만 적용
+//    private Integer quantity;
 
     public Item() {
     }
