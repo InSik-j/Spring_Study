faecc168d83759c6fd65c4b8d66bc4c71ad95df4
Insik Jeong <150884733+InSik-j@users.noreply.github.com>
2024-07-16 15:08:06 +0900

    데이터 접근 기술 - 스프링 JdbcTemplate / JdbcTemplate - 이름 지정 파라미터 1

--- spring-db2/itemservice-db/src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV2.java
+++ spring-db2/itemservice-db/src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV2.java
@@ -48,7 +48,7 @@ public class JdbcTemplateItemRepositoryV2 implements ItemRepository {
     @Override
     public Item save(Item item) {
         String sql = "insert into item (item_name, price, quantity) " +
-                "values (:itemName, :price, quantity)";
+                "values (:itemName, :price, :quantity)";
 
         SqlParameterSource param = new BeanPropertySqlParameterSource(item);
         KeyHolder keyHolder = new GeneratedKeyHolder();
