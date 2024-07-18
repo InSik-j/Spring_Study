594df2387cb523c61e617bccd41606cdf9c74c6b
Insik Jeong <150884733+InSik-j@users.noreply.github.com>
2024-07-18 11:10:59 +0900

    데이터 접근 기술 - JPA / 적용1 - 개발

--- spring-db2/itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ spring-db2/itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -2,11 +2,18 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
 @Data
+@Entity
 public class Item {
 
+    @Id @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
+    @Column(name = "item_name", length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
